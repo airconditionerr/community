28fb3be87539e8c00ebecc53b044038044ffb8ab
AirConditioner <291758746@qq.com>
2022-09-12 16:12:14 +0800

    login without refresh

--- src/main/resources/static/js/question.js
+++ src/main/resources/static/js/question.js
@@ -15,7 +15,15 @@ function post() {
             if (response.code == 200){
                 $ ("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code == 2003){
+                    var message = confirm(response.message);
+                    if (message){
+                        window.open("https://github.com/login/oauth/authorize?client_id=07321f78eebe9dfe4011&redirect_uri=http://localhost:8888/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
         }
     });
